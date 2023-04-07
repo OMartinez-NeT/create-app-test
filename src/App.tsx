f9456f3ae83462cd67a0e635bc83a21b476e1aab
“Oscar <oscar@Oscars-MacBook-Pro.local>
2023-04-07 18:58:58 -0400

    Just testing

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Version 2 app
+          Video demo
         </a>
       </header>
     </div>
